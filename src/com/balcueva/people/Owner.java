e1a52c96ad6d7275a5f59dbaa586c6e46d030427
Mark John Balcueva <75126648+mjbalcueva@users.noreply.github.com>
2022-09-19 00:51:56 +0800

    feat: add ability to have pets
    owner objects are now able to have pets.

--- src/com/balcueva/people/Owner.java
+++ src/com/balcueva/people/Owner.java
@@ -1,16 +1,55 @@
 package com.balcueva.people;
 
+import java.util.ArrayList;
 import com.balcueva.date.MyDate;
+import com.balcueva.pets.Pet;
 import com.balcueva.utils.Enums.Gender;
 
 public class Owner extends Person {
+  private ArrayList<Pet> pets = new ArrayList<>();
+  private String pronoun = gender.equals(Gender.MALE) ? "He" : "She";
+
   public Owner(MyDate birthDate, Gender gender, String firstName, String lastName, String contact) {
     super(birthDate, gender, firstName, lastName, contact);
   }
 
+  public void addNewPets(Pet... pets) {
+    for (Pet pet : pets) {
+      this.pets.add(pet);
+    }
+  }
+
+  public String noiseInOwnersHouse() {
+    StringBuilder noise = new StringBuilder();
+    if (pets.isEmpty()) {
+      noise.append(fullName + "'s house is quiet because ");
+      noise.append(pronoun.toLowerCase() + " doesn't have any pets");
+    } else {
+      noise.append(pronoun + "'s house is noisy. Listen...");
+      for (Pet pet : pets) {
+        noise.append("\n\t- " + pet.speak());
+      }
+    }
+    noise.append("\n");
+    return noise.toString();
+  }
+
   @Override
   public String toString() {
-    return fullName + " is an owner" + "\n\tContact information:" + "\n\t\tPersonal Id: " + personID
-        + "\n\t\tDate of Birth: " + birthDate;
+    StringBuilder owner = new StringBuilder();
+    owner.append(fullName + " is an owner");
+    owner.append("\n\tContact information: ");
+    owner.append("\n\t\t- Personal Id: " + personID);
+    owner.append("\n\t\t- Date of Birth: " + birthDate);
+    if (pets.isEmpty()) {
+      owner.append("\n\t\t- " + pronoun + " doesn't have any pets");
+    } else {
+      owner.append("\n\t\t- " + pronoun + " has " + pets.size() + " pets:");
+      for (Pet pet : pets) {
+        owner.append("\n\t\t\t~ " + pet);
+      }
+    }
+    owner.append("\n");
+    return owner.toString();
   }
 }
