72f700f4a65851c6cde1328468fda37f2fa1b386
Mark John Balcueva <75126648+mjbalcueva@users.noreply.github.com>
2022-09-24 12:53:49 +0800

    refactor: create an instance from Pet

--- src/com/balcueva/App.java
+++ src/com/balcueva/App.java
@@ -6,6 +6,7 @@ import com.balcueva.people.Owner;
 import com.balcueva.pets.Cat;
 import com.balcueva.pets.Dog;
 import com.balcueva.pets.Parrot;
+import com.balcueva.pets.Pet;
 import com.balcueva.pets.Turtle;
 import com.balcueva.utils.Enums.Gender;
 import com.balcueva.utils.Enums.PetType;
@@ -14,13 +15,13 @@ import com.balcueva.utils.Util;
 public class App {
   public static void main(String[] args) {
     // part one, create owners
-    Owner owner1 = new Owner(new MyDate(1997, 7, 8), Gender.FEMALE, "Anne", "SMITH", "+40-741-234-567");
+    Owner owner1 = new Owner(new MyDate(1997, 7, 8), Gender.FEMALE, "Anne", "Smith", "+40-741-234-567");
     System.out.println(owner1);
     System.out.println(owner1.noiseInOwnersHouse());
-    Cat pet1 = new Cat(true, MyDate.randomDate(2018, 2020), "Joe", Util.getRandom().nextDouble(7));
-    Dog pet2 = new Dog(true, MyDate.randomDate(2018, 2020), "Lizzie", Util.getRandom().nextDouble(6));
-    Parrot pet3 = new Parrot(true, MyDate.randomDate(2018, 2020), "Pityu", Util.getRandom().nextDouble(5));
-    Turtle pet4 = new Turtle(5, MyDate.randomDate(2018, 2020), "Tim", Util.getRandom().nextDouble(8));
+    Pet pet1 = new Cat(true, MyDate.randomDate(2018, 2020), "Joe", Util.getRandom().nextDouble(7));
+    Pet pet2 = new Dog(true, MyDate.randomDate(2018, 2020), "Lizzie", Util.getRandom().nextDouble(6));
+    Pet pet3 = new Parrot(true, MyDate.randomDate(2018, 2020), "Pityu", Util.getRandom().nextDouble(5));
+    Pet pet4 = new Turtle(5, MyDate.randomDate(2018, 2020), "Tim", Util.getRandom().nextDouble(8));
     owner1.addNewPets(pet1, pet2, pet3, pet4);
     System.out.println(owner1);
     System.out.println(owner1.noiseInOwnersHouse());
@@ -35,6 +36,6 @@ public class App {
     // part three, pet gets sick
     owner1.getPet("Lizzie").unexpectedIllnessOccured();
     System.out.println(owner1.getPet("Lizzie"));
-    owner1.getPet("Lizzie").checkHealthStatus();;
+    owner1.getPet("Lizzie").checkHealthStatus();
   }
 }
